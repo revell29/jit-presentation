5c47758f423109bf4ead22cb16346e4f13027302
apsya Dira <diraapsya0@gmail.com>
2022-12-02 14:59:15 +0700

    init commit

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+// example Layout.js
+import React from "react";
+
+export default ({ children }) => (
+  <div
+    style={{
+      width: "100vw",
+      height: "100vh",
+    }}
+  >
+    {children}
+  </div>
+);
